918d7a1f2b8de733ef397fa2aa7cd2920e106b3a
Troy Adair <troy.adair@datadicts.com>
2018-06-15 13:05:00 -0400

    Start of Intro to R script

---
+++ Intro to R.R
@@ -0,0 +1,14 @@
+# Asignment Example
+x = 7
+y <- 8
+#
+Name <- "Troy"
+CheckFlag <- T
+DateToday <- date()
+#
+# use as.Date( ) to convert strings to dates
+mydates <- as.Date(c("2007-06-22", "2004-02-13"))
+mydates
+# number of days between 6/22/07 and 2/13/04
+days <- mydates[1] - mydates[2]
+days
