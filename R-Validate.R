f1727a27020446d0e9135af57a46b84f5ac81a28
Troy Adair <troy.adair@datadicts.com>
2018-06-21 12:26:59 -0400

    mutate()

--- R-Validate.R
+++ R-Validate.R
@@ -18,11 +18,16 @@ cf <- confront(Yellow_Tripdata_2017_06,v)
 cf
 summary(cf)
 Yellow_Tripdata_2017_06$tpep_dropoff_datetime <- as.POSIXct(Yellow_Tripdata_2017_06$tpep_dropoff_datetime,
-                                      format='%Y-%m-%d %H:%M')
+                                    format='%Y-%m-%d %H:%M')
+Yellow_Tripdata_2017_06$tpep_pickup_datetime <- as.POSIXct(Yellow_Tripdata_2017_06$tpep_pickup_datetime,
+                                    format='%Y-%m-%d %H:%M')
 View(Yellow_Tripdata_2017_06)
 str(Yellow_Tripdata_2017_06)
 
 install.packages("dplyr")
 library(dplyr)
-mutate(Yellow_Tripdata_2017_06,speed=trip_distance/(tpep_dropoff_datetime-tpep_pickup_datetime))
-       
+workdata <- mutate(Yellow_Tripdata_2017_06,trip_length=(tpep_dropoff_datetime-tpep_pickup_datetime)/60)
+workdata$trip_length <- as.integer(workdata$trip_length)
+workdata <- mutate(workdata,trip_speed=trip_distance*60/trip_length)
+
+View(workdata)       
