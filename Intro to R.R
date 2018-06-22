db05a3b180194086a9d76ec2dea197bba949c712
Troy Adair <troy.adair@datadicts.com>
2018-06-21 22:40:52 -0400

    Intro to R and R-Validate scripts consolidated.

--- Intro to R.R
+++ Intro to R.R
@@ -1,32 +1,37 @@
 # R script to accopany Intro to R
 #######################################################
+
 # Assignment Examples
 # Numeric
 x = 7
 y <- 8
-#
+
+
 # Character
-#
 Name <- "Troy"
-#
+
+
 # Logical
-#
 CheckFlag <- T
-#
+
 #Date 
 DateTime <- date()
 Date <- Sys.Date()
+
 # use as.Date( ) to convert strings to dates
 mydates <- as.Date(c("2007-06-22", "2004-02-13"))
 mydates
+
 # number of days between 6/22/07 and 2/13/04
 days <- mydates[1] - mydates[2]
 days
-#
+
+
 # Operators
-#
+
 z <- X + y
 # The preceeding will return an error becuase X <> x
+
 z1 <- x + y
 z1
 z2 <- x - y
@@ -39,127 +44,128 @@ z5 <- x ** y
 z5
 z6 <- x ^ y
 z6
-#
+
+
 # How to programmatically remove all variables
-#
 rm(list=ls(all=TRUE))
-#
+
 # How to programmatically clear the Console
 cat("\014")
-#
+
+
 # Vectors in R
-#
 name <- c("Mike", "Lucy", "John") 
 name[1]
 name[2:3]
 name[c(1,3)]
-#
+
 age <- c(20, 25, 30) 
+
 #
 X <- c(1, 3, 5, 7, 9, 11)
 X*3
 sqrt(X)
+
 Y <- c(2,4,6,8,10,12)
 X-Y
+
 Z <- c(0,2,4)
+
 X-Z
+
 Z2 <- c(0,1,2,3)
+
 X-Z2
-#
+
+
 # Missing Data in R
-#
 Z <- c(2, NA, 6, 8, NA, 12)
 is.na(Z)
+
 Z <- c(2, NULL, 6, 8, NA, 12)
-#
+
 # Useful Functions....
-#
 nchar(name)
 length(X)
 mean(X)
-?`*`
 ?"*"
 apropos('mea')
+
 # Clean Up 
-#
 rm(list=ls(all=TRUE))
 cat("\014") 
-#
-#
+
 # Arrays and Matrices
-#
+
 theArray2d <- array(1:9, dim=c(3,3))
 theArray2d[1,2]
-#
+
 View(theArray2d)
-#
+
 theArray2d <- array(1:9, dim=c(3,3), byrow=TRUE)
 # The above is to show that byrow is not valid for arrays
+
 View(theArray2d)
-#
+
 theArray3d <- array(1:27, dim=c(3,3,3))
+
 theArray3d[1,2,3]
-#
 View(theArray3d)
-#
+
 theMatrix <- matrix(1:4, nrow=2)
 View(theMatrix)
-#
+
 theMatrix <- matrix(1:4, nrow=2, byrow=TRUE)
 View(theMatrix)
+
 # Lists
 y <- list("a", 1L, 1.5, TRUE)
 y[3]
 str(y)
 ?str()
-#
+
 # Clean Up 
-#
 rm(list=ls(all=TRUE))
 cat("\014") 
-##
+
 # Data Frames
-#
 x <- 10:1
 y <- -4:5
 z <- c('Hockey', 'Football', 'Curling', 'Soccer', 'Rugby', 'Baseball', 'Golf', 'Basketball', 'Wrestling', 'Tennis')
 theDF <- data.frame(x,y,z)
 theDF
+
 theDF[1,2]
+
 View(theDF)
-#
+
 names(theDF)<-c("Popularity", "Team Strength", "Sport")
 View(theDF)
-#
+
 theDF[2:3]
 theDF[1,3]
 theDF[c(1,3)]
 theDF[3]
 theDF$Sport
 theDF$`Team Strength`
-#
+
 # Clean Up 
-#
 rm(list=ls(all=TRUE))
 cat("\014") 
 #
-# Yellow_Tripdata_2017_06 <- read.csv('yellow_tripdata_2017-06.csv')
+Yellow_Tripdata_2017_06 <- read.csv('yellow_tripdata_2017-06.csv')
 View(Yellow_Tripdata_2017_06)
+
 head(Yellow_Tripdata_2017_06)
 str(Yellow_Tripdata_2017_06)
 summary(Yellow_Tripdata_2017_06)
+
 #
-#Filtered_Yellow_Tripdata_2017_06 <- filter(Yellow_Tripdata_2017_06,Yellow_Tripdata_2017_06[5]<1000)
-#View(Filtered_Yellow_Tripdata_2017_06)
-#str(Filtered_Yellow_Tripdata_2017_06)
-#summary(Filtered_Yellow_Tripdata_2017_06)
-#
-#Filtered_Yellow_Tripdata_2017_06<-Yellow_Tripdata_2017_06[5]<1000
-#str(Filtered_Yellow_Tripdata_2017_06)
-#summary(Filtered_Yellow_Tripdata_2017_06)
-#
-d <- Yellow_Tripdata_2017_06
-d2<-d[which(d$trip_distance<1000),]
+# d <- Yellow_Tripdata_2017_06
+d2<-Yellow_Tripdata_2017_06[which(Yellow_Tripdata_2017_06$trip_distance<1000),]
 
 str(d2)
 summary(d2)
+
+# Let's save our data frame for the next module...
+save(Yellow_Tripdata_2017_06,file="Yellow_Tripdata_2017_06.RData")
--- R-Validate.R
+++ R-Validate.R
@@ -1,28 +1,47 @@
+# Load the previously referenced data frame in "Yellow_Tripdata_2017_06.RData"
+load("Yellow_Tripdata_2017_06.RData")
+
+# Note: "install" will throw warning errors if a package has been previously ignored
 install.packages("validate")
+
+# Load "validate" package into active memory
 library(validate)
 ?validate()
+
+# Let's examine Yellow_Tripdata_2017_06 to remind us what's in it...
 View(Yellow_Tripdata_2017_06)
 names(Yellow_Tripdata_2017_06)
-# To add a new variable with sequential IDs
+
+# Let's add a new variable with sequential IDs for each row
 Yellow_Tripdata_2017_06$id<-seq.int(nrow(Yellow_Tripdata_2017_06))
-#
+
+# Now we'll check to see that the new "id" variable has been added...
 names(Yellow_Tripdata_2017_06)
+
 # Trick to list column names, numbered and vertically
-c<-colnames(Yellow_Tripdata_2017_06)
-as.data.frame(c)
+as.data.frame(colnames(Yellow_Tripdata_2017_06))
+
 # attach() will place the data frame name into the search path, meaning that
-# for many functions can dispense with using the whole data frame name.
+# for many functions we can dispense with using the whole data frame name.
 attach(Yellow_Tripdata_2017_06)
-#
+
+# Now we can actually start using the validate package.
+# validator() will take desired rules as inputs...
 v <-  validator( trip_distance<1000,total_amount>0,trip_distance>0)
 v
 
+# Now, we "confront()" our data with that set of rules, specifying our unique key
 cf <- confront(Yellow_Tripdata_2017_06,v,key="id")
 cf
+
+# Let's put the cf into a data frame object to see fi we can see more about it...
 out <- as.data.frame(cf)
 View(out)
+
 # If we merge this with the original data, will get >28.9M rows...why?
-# So, alternate way to wind up with a tractable data set
+# So, alternate way to wind up with a tractable data set...
+# (Note: we'll see a much better way to handle this with the dplyr() package later)
+
 v1<-validator(trip_distance<1000)
 v1
 cf1<-confront(Yellow_Tripdata_2017_06,v1,key="id")
@@ -49,15 +68,24 @@ names(out1)
 names(out1)[3]<-"TD.LT.1000"
 names(out2)[3]<-"AMT.GT.0"
 names(out3)[3]<-"TD.GT.0"
+
 # Now to merge the results of the confrontation with Yellow_Tripdata_2016_06
+# We'll start with "out1"....
 YT_2017_06_Validated <- merge(out1,Yellow_Tripdata_2017_06, by.x="id",by.y="id")
+# " ... and we'll use "subset" to drop "name" and "expression"...
 YT_2017_06_Validated <- subset(YT_2017_06_Validated,select=-c(name,expression))
 
+# Next, merge "out2" into our (working) merged data frame, YT_2016_06_Validated....
 YT_2017_06_Validated <- merge(out2,YT_2017_06_Validated, by.x="id",by.y="id")
 YT_2017_06_Validated <- subset(YT_2017_06_Validated,select=-c(name,expression))
 
+# Finally, merge "out3" into our (working) merged data frame, YT_2016_06_Validated....
 YT_2017_06_Validated <- merge(out3,YT_2017_06_Validated, by.x="id",by.y="id")
 YT_2017_06_Validated <- subset(YT_2017_06_Validated,select=-c(name,expression))
 
+# And let's take a look at what it looks like:
 View(YT_2017_06_Validated)
 str(YT_2017_06_Validated)
+
+# Finally, save our validated data set for the next module...
+save(YT_2017_06_Validated,file="YT_2017_06_Validated.RData")
