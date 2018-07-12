7c63df7f0e8870cd1a57f4dc95bbf8249d897294
Troy Adair <troy.adair@datadicts.com>
2018-07-12 12:58:31 -0400

    RCmdr initial commit

--- RCmdr.R
+++ RCmdr.R
@@ -1,16 +1,5 @@
-# Install and load Rcmdr package
+install.packages("car")
+install.packages("haven")
 install.packages("Rcmdr")
 library(Rcmdr)
-
-# Install and load RMariaDB package
-install.packages("RMariaDB")
-library(RMariaDB)
-
-#Use dbConnect to construct connection information
-con <- dbConnect(RMariaDB::MariaDB(), dbname='test', user = "root", password = "root2018")
-
-# Which tables?
-dbListTables(con)
-
-# Send query to MariaDB, retrieve results, and clear the result set, all in one step
-sample <- dbGetQuery(con, 'SELECT * FROM YT_VALIDATED_5PCT')
+load("YT_2017_06_Validated.RData")
